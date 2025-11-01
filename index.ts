c2afdf01bb270a5a7e3ca9beca49a49252c4be99
shimul <mdshimulhossen517@gmail.com>
2025-11-02 00:49:08 +0600

    added server and route

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
 
-const Post = ( text: string) => {
-    return text
-}
+import figlet from "figlet";
 
-const get = Post("HELLO WORLD@!")
-console.log(get);
+const server = Bun.serve({
+    port:3001,
+    fetch(req){
+        const url = new URL(req.url)
+        if(url.pathname === "/"){
+            const msg = figlet.textSync("HOLA")
+            return new Response (msg)
+        }
+        if (url.pathname === "/about"){
+            return new Response ("About Me")
+        }
+        if(url.pathname === "/contact"){
+            return new Response ("Contact Us")
+        }
+        return new Response ("404 Not Found")
+        
+        
+    }
+});
+
+console.log(`Listenig on PORT http://localhost:${server.port}`);
+
